3c0ee58edd200a9b5fd8a7698a0b4c29fc3fed35
Vizena2005 <vizena2005@gmail.com>
2018-08-08 10:34:56 -0500

    final push prior to polished heroku upload

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 // DB Config
